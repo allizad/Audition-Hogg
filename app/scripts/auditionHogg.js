c5a7322be7f1dbabb31108cf2ed7bf60d5d80d62
Allison Zadrozny <allison@zadrozny.com>
2014-08-14 17:49:18 -0500

    Added views and ng-route on main page working. Fuck yeah.

--- app/scripts/auditionHogg.js
+++ app/scripts/auditionHogg.js
@@ -1,3 +1,27 @@
+
 'use strict';
 
-angular.module('auditionHogg', []);
+angular.module('auditionHogg', ['ngRoute', 'firebase'])
+	.config(function ($routeProvider, $locationProvider) {
+	    $routeProvider
+	    	.when('/subscribe', {
+	    		templateUrl: "../views/subscribe.html",
+	    		controller: ''
+	    	})
+	    	.when('/pl', {
+	            templateUrl: '../views/postListing.html',
+	            controller: ''
+	        })
+	        .when('/sl', {
+	        	templateUrl: "../views/searchListing.html",
+	        	controller: ''
+	        })
+	        .when('/about', {
+	        	templateUrl: "../views/about.html",
+	        	controller: ''
+	        })
+	        .otherwise({
+	            redirectTo: '/'
+	        });
+	    // $locationProvider.html5Mode(true);
+	});
--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('auditionHogg').controller('MainCtrl', function($scope, $firebase) {
-  var ref = new Firebase('https://mkshackathon.firebaseio.com/');
-  var sync = $firebase(ref);
-  // create a synchronized array for use in our HTML code
-  $scope.messages = sync.$asArray();
+  // var ref = new Firebase('https://mkshackathon.firebaseio.com/');
+  // var sync = $firebase(ref);
+  // // create a synchronized array for use in our HTML code
+  // $scope.messages = sync.$asArray();
 
-  $scope.addMessage = function(text) {
-    $scope.messages.$add({text: text});
-  };
+  // $scope.addMessage = function(text) {
+  //   $scope.messages.$add({text: text});
+  // };
+});
+
+angular.module('auditionHogg').controller('formCtrl', function($scope){
+	$scope.text = 'me@example.com'
 });
