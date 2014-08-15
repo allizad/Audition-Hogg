46a520aaa8daf2e5f57ae985a145da63f36dbfe0
Quin Adney <quinlan.adney@gmail.com>
2014-08-15 00:23:32 -0500

    first draft angularfire logic

--- app/scripts/auditionHogg.js
+++ app/scripts/auditionHogg.js
@@ -1,4 +1,3 @@
-
 'use strict';
 
 angular.module('auditionHogg', ['ngRoute', 'firebase'])
@@ -24,4 +23,31 @@ angular.module('auditionHogg', ['ngRoute', 'firebase'])
 	            redirectTo: '/'
 	        });
 	    // $locationProvider.html5Mode(true);
-	});
+	})
+
+	.controller("Firetube", ["$scope", "$firebase", 
+		function($scope, $firebase) {
+		var ref = new Firebase("https://mkshackathon.firebaseio.com/Postings/All");
+		var sync = $firebase(ref);
+
+		$scope.comments = sync.$asArray();
+		$scope.username = 'Guest' + Math.floor(Math.random() * 101)
+
+		$scope.addComment = function(e) {
+			if (e.keyCode != 13) return;
+			console.log($scope.comments)
+
+			$scope.comments.$add({
+				from: $scope.username,
+				body: $scope.newComment
+			});
+			$scope.newComment = "";
+			}
+		}
+	]);
+
+// app.controller("PostController", function() {
+// 	this.
+// })
+
+// angular.module("firetube", ["firebase"])
